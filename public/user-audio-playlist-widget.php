09201f96dc292ffee0b4b866cc7e6a742cbc3e67
Mike Starov <mike.starov@gmail.com>
2015-02-05 20:39:35 -0800

    Added some files for CUE integration

--- public/user-audio-playlist-widget.php
+++ public/user-audio-playlist-widget.php
@@ -23,6 +23,7 @@ class User_Audio_Playlist_Widget extends WP_Widget {
 		);
 	}
 
+	
 	/**
 	 * Front-end display of widget.
 	 *
@@ -32,6 +33,18 @@ class User_Audio_Playlist_Widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
+		$this->widget_simple($args, $instance);
+	}
+	
+	/**
+	 * Simple widget display
+	 * 
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget_simple( $args, $instance ) {
 	
      	        echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
@@ -66,6 +79,69 @@ END;
 		}
 		echo "<div/>";
 		
+		echo $args['after_widget'];
+	}
+	
+	/**
+	 * Cue playlist widget display
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget_cue( $args, $instance ) {
+	
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+	
+		$playlist = new Playlist_Manager(UAP_PLAYLIST_SLUG);
+		$parray = $playlist -> as_array();
+	
+		echo "<div class='".UAP_SLUG."' id='playlist-".$parray['slug']."'>";
+		echo "<h4 class='user_audio_playlist-title'>".$parray['title']."</h4>";
+		if(empty($parray['items']))
+			echo "<p>".__('Playlist is empty.')."</p>";
+		else
+		{
+			// Show cue playlist
+			$classes   = array( 'cue-playlist' );
+			$classes[] = ( isset( $args['show_playlist'] ) && false == $args['show_playlist'] ) ? 'is-playlist-hidden' : '';
+			$classes   = implode( ' ', array_filter( $classes ) );
+			
+			echo '<div class="cue-playlist-container">';
+			
+			//do_action( 'cue_before_playlist', $post, $tracks, $args );
+			
+			$tracks = $parray['items'];
+			
+			include( plugin_dir_path( dirname( __FILE__ ) ).'/templates/playlist.php' );
+			
+			//do_action( 'cue_after_playlist', $post, $tracks, $args );
+			
+			echo '</div>';
+			
+// 			echo "<ul>";
+// 			//TODO: Use common values for class and data action in remove link
+// 			//TODO: Sanitize attributes, etc
+// 			foreach ($parray['items'] as $key => $item) {
+// 				echo <<<END
+// 					<audio class="wp-audio-shortcode" id="" preload="none"
+// 				        style="width: 100%; visibility: hidden;" controls="controls">
+// 				        <source type="audio/mpeg" src="$item?_=1"/>
+// 				        <a href="$item">$item</a>
+// 				    </audio>
+// 				    &nbsp;
+// 				    <a href="#" class="remove-from-playlist" data-plitemkey="$key" data-plitem="$item" data-action="remove_from_playlist" data-pltitle="{$parray['title']}">remove</a>
+// 				    <hr/>
+// END;
+// 			}
+// 			echo "</ul>";
+		}
+		echo "<div/>";
+	
 		echo $args['after_widget'];
 	}
 
